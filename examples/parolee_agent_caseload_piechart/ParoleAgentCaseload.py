c3d1823e286181c7e199be26c460745acf75fe90
Carlos P <proto@Carloss-MBP.attlocal.net>
2020-10-03 04:59:50 -0700

    analysis scripts

--- examples/parolee_agent_caseload_piechart/ParoleAgentCaseload.py
+++ examples/parolee_agent_caseload_piechart/ParoleAgentCaseload.py
@@ -8,10 +8,10 @@ import pandas as pd
 import numpy as np
 
 # read in parolee 2018 age data
-df = pd.read_csv("docs/data/parole_age.csv")
+df = pd.read_csv("../../docs/data/parole_agent_caseload.csv")
 print(df)
 
 # plot distribution on pie chart
-fig = px.pie(df, values='population_perc', names='age', color_discrete_sequence=px.colors.sequential.RdBu)
-fig.write_html("parole_age.html")
+fig = px.pie(df, values='stat_perc', names='status', color_discrete_sequence=px.colors.sequential.RdBu)
+fig.write_html("parole_agent_caseload.html")
 fig.show()
---
+++ examples/parolee_ethnicity_piechart/ParoleEthnicity.py
@@ -0,0 +1,17 @@
+# import all basic libraries
+from urllib.request import urlopen
+import json
+import plotly.figure_factory as ff
+import plotly.express as px
+import plotly.graph_objs as go
+import pandas as pd
+import numpy as np
+
+# read in parolee 2018 age data
+df = pd.read_csv("../../docs/data/parole_ethnicity.csv")
+print(df)
+
+# plot distribution on pie chart
+fig = px.pie(df, values='eth_perc', names='ethnicity', color_discrete_sequence=px.colors.sequential.RdBu)
+fig.write_html("parole_ethnicity.html")
+fig.show()
